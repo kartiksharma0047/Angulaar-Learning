d05131d25704baed106356b827a77b1b80920139
kartiksharma0047 <kartiksharma55109@gmail.com>
2026-03-30 10:01:03 +0530

    Made Reactive Form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,55 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  employees: any[] = [
-    { id: 1, name: 'Amit' },
-    { id: 2, name: 'Ankit' },
-    { id: 3, name: 'Raj' },
-    { id: 4, name: 'Rahul' },
-    { id: 5, name: 'Ravi' },
-  ];
+export class App implements OnInit {
+  userForm!: FormGroup;
+  Validators = Validators;
+  constructor(private fb: FormBuilder) {}
 
-  isExist:boolean=true
+  ngOnInit(): void {
+    console.log('Form is Online For submission');
+    this.initializeForm();
+  }
+  initializeForm() {
+    this.userForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: [''],
+      email: ['', [Validators.required, Validators.email]],
+      contact: [''],
+    });
+  }
 
-  CheckStatus(){
-    this.isExist=!this.isExist
+  getfname() {
+    return this.userForm.get('fname');
+  }
+  getlname() {
+    return this.userForm.get('lname');
+  }
+  getEmail() {
+    return this.userForm.get('email');
+  }
+  getContact() {
+    return this.userForm.get('contact');
+  }
+
+  onSubmit() {
+    if (this.userForm.invalid) {
+      this.userForm.markAllAsTouched();
+      console.log('Form Is InValid');
+      console.log(this.userForm);
+      console.log(this.getfname)
+      return;
+    }
+
+    console.log(this.userForm.value);
   }
 }
